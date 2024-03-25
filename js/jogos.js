6f58b56999aaf8f4c41fabd2034374d300a8c9b3
GiuliannoMonteiro <155036429+GiuliannoMonteiro@users.noreply.github.com>
2024-03-24 22:22:51 -0300

    Add files via upload

---
+++ js/jogos.js
@@ -0,0 +1,24 @@
+const jogos = [
+    {
+        id: "0000001",
+        nome: "Empty Place",
+        image: "https://i.postimg.cc/TwPj7Fs5/image0-waifu2x-art-noise3.png",
+        vercao: "1.0.0",
+        descricao: "Um jogo de plataforma 2D side-scroller onde o jogador está em uma nave espacial infestada por vários aliens, referidos como criptomorfos. Quanto mais o jogador avança, mais claustofóbicas as fases ficam e mais tipos de aliens são introduzidos. O jogador iráencontrar diferentes armas durante seu caminho que poderá usar contra os criptomorfos.O jogo terá 10 fases, um diferente da outra. No meio da fase 5, a energia da nave irá cair e asfracas luzes apagam de uma vez. No final, o jogador terá que enfrentar a rainha alien. Após isso, vários jogos irão chocar e o jogador terá que correr em direção à sala de controle, com uma chave. Ao virar a chave, o jogador ativa a sequência de auto-destruição, devendo correr em direção ao módulo de escape. A virada de chave fará tocar um alto som de uma sirene constante. Assim, podefugir, enquanto a nave explode com os aliens dentro.",
+        preco: "Grátis",
+        genero: "Terror, sobrevivência,Tiro, 2D",
+        tipo: "2D"
+    },
+    {
+        id: "0000002",
+        nome: "Defcon 0",
+        image: "https://i.postimg.cc/25VfBKYC/0-1.png",
+        vercao: "1.0.0",
+        descricao: "No jogo dois jogadores se enfrentam, um representando a União Soviética e o outro, os Estados Unidos. Uma partida consiste em ataque, defesa, realização de inteligência, contrainteligência, recebimento de reforços de países aliados e a construção de bases em locais estratégicos. Cada jogador tem 3 linhas para pôr cartas, cada uma com 3 espaços para as cartas. A linha de cima contém as cartas da sua agência de inteligência, a do meio contém cartas viradas para baixo contendo a informação de que cidades bases militares do respectivo país foram construídas. São nessas bases que mísseis balísticos intercontinentais são lançados.",
+        preco: "Grátis",
+        genero: "Guerra, ação e estratégia",
+        tipo: "Tabuleiro"
+    }
+];
+
+export default jogos;
